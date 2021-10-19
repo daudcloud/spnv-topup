fcfafcc6b85c80db063d444e2d021ef45bf72cd1
unknown <daudsutanto13@gmail.com>
2021-10-19 14:05:17 +0700

    adding home button

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -2,8 +2,15 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="py-9 flex justify-between items-center">
-      <h1 className="text-4xl text-gray-700">SPNV Topups</h1>
+    <div className="py-9 flex items-center">
+      <Link href="/">
+        <a className="text-4xl text-gray-700">SPNV Topups</a>
+      </Link>
+      <Link href="/">
+        <a className="ml-auto font-bold rounded-md p-2 px-4 hover:bg-gray-100 cursor-pointer transition-all mr-4 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-opacity-75">
+          Home
+        </a>
+      </Link>
       <Link href="/cara-bayar">
         <a className="btn-main">Cara Bayar</a>
       </Link>
